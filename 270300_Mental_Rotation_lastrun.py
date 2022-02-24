48c7a317547cebf85ba03be1ef303e046be6bbd9
cAlting <96538752+cAlting@users.noreply.github.com>
2022-02-24 09:56:33 +0100

    3D-Task-Instruktionen hinzugefügt
    Änderungen:
    - Keyboard-Komponente der instructions_2D-Routine in instr_2D_kb umbenannt
    
    Hinzugefügt:
    - instructions_3D-Routine
        - instr_3D-Text (Units: norm, um durch Abschätzung des durch den Text in der Aufgabenstellung (Abb. 4) eingenommenen Bildschirmteils schnell die Abb. 4 nachbilden zu können)
        - instr_3D_kb (Keyboard-Komponente, um die 3D-Instruktionen durch Leertastendruck beenden zu können)
    
    ZU TESTZWECKEN:
    - Anzahl Wiederholungen des Loops 'trials_2D' auf 0 gesetzt, um 2D-Task überspringen zu können

--- 270300_Mental_Rotation_lastrun.py
+++ 270300_Mental_Rotation_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on Februar 24, 2022, at 09:29
+    on Februar 24, 2022, at 09:51
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -65,7 +65,7 @@ frameTolerance = 0.001  # how close to onset before 'same' frame
 
 # Setup the Window
 win = visual.Window(
-    size=[3440, 1440], fullscr=True, screen=1, 
+    size=[1920, 1080], fullscr=True, screen=1, 
     winType='pyglet', allowGUI=False, allowStencil=False,
     monitor='testMonitor', color='black', colorSpace='rgb',
     blendMode='avg', useFBO=True, 
@@ -148,6 +148,16 @@ img_wrong = visual.ImageStim(
     color=[1,1,1], colorSpace='rgb', opacity=None,
     flipHoriz=False, flipVert=False,
     texRes=128.0, interpolate=True, depth=-4.0)
+
+# Initialize components for Routine "instructions_3D"
+instructions_3DClock = core.Clock()
+text = visual.TextStim(win=win, name='text',
+    text='Die folgenden Figuren müssen verglichen werden. Überlappen die Figuren durch Drehen vollständig, dann drücken Sie die Pfeiltaste nach OBEN, wenn nicht, drücken Sie die Pfeiltaste nach UNTEN. \n\nZum Fortfahren bitte LEERTASTE drücken.',
+    font='Open Sans',
+    units='norm', pos=(0, 0), height=0.1, wrapWidth=1.2, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
 
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
@@ -250,7 +260,7 @@ thisExp.nextEntry()
 routineTimer.reset()
 
 # set up handler to look after randomisation of conditions etc
-trials_2D = data.TrialHandler(nReps=15.0, method='random', 
+trials_2D = data.TrialHandler(nReps=0.0, method='random', 
     extraInfo=expInfo, originPath=-1,
     trialList=[None],
     seed=None, name='trials_2D')
@@ -329,8 +339,8 @@ for thisTrials_2D in trials_2D:
         elif fix_point_ISI.status == STARTED:  # one frame should pass before updating params and completing
             # updating other components during *fix_point_ISI*
             img_correct.setImage(img_correct_path)
-            img_target.setImage('Stimuli_2d/target' + str(img_index[trials_2D.thisN]) + '.png')
-            img_wrong.setImage('Stimuli_2d/wrong' + str(img_index[trials_2D.thisN]) + '.png')
+            img_target.setImage(img_target_path)
+            img_wrong.setImage(img_wrong_path)
             # component updates done
             fix_point_ISI.complete()  # finish the static period
             fix_point_ISI.tStop = fix_point_ISI.tStart + 1  # record stop time
@@ -538,6 +548,9 @@ for thisTrials_2D in trials_2D:
     thisExp.addData('x_pos_wrong', -x_pos_correct)
     
     # Speichern der verwendeten Bilddateien
+    # (in den csv-Dateien kann man anhand der Zahlern
+    # der verwendeten Bilder sehen, dass die Stimuli
+    # randomisiert präsentiert werden)
     thisExp.addData('img_correct', img_correct_path)
     thisExp.addData('img_wrong', img_wrong_path)
     thisExp.addData('img_target', img_target_path)
@@ -560,8 +573,70 @@ for thisTrials_2D in trials_2D:
     trials_2D.addData('img_wrong.stopped', img_wrong.tStopRefresh)
     thisExp.nextEntry()
     
-# completed 15.0 repeats of 'trials_2D'
+# completed 0.0 repeats of 'trials_2D'
 
+
+# ------Prepare to start Routine "instructions_3D"-------
+continueRoutine = True
+# update component parameters for each repeat
+# keep track of which components have finished
+instructions_3DComponents = [text]
+for thisComponent in instructions_3DComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+instructions_3DClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+
+# -------Run Routine "instructions_3D"-------
+while continueRoutine:
+    # get current time
+    t = instructions_3DClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=instructions_3DClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text* updates
+    if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        text.frameNStart = frameN  # exact frame index
+        text.tStart = t  # local t and not account for scr refresh
+        text.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+        text.setAutoDraw(True)
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in instructions_3DComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "instructions_3D"-------
+for thisComponent in instructions_3DComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('text.started', text.tStartRefresh)
+thisExp.addData('text.stopped', text.tStopRefresh)
+# the Routine "instructions_3D" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
 
 # Flip one final time so any remaining win.callOnFlip() 
 # and win.timeOnFlip() tasks get executed before quitting
